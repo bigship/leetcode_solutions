c2af66d49db8a92f8664b018b77dfd9056802aa1
bigship <colinchen1984@gmail.com>
2018-09-13 21:36:50 +0800

    rename filename to problem description

---
+++ validateBinarySearchTree.cpp
@@ -0,0 +1,50 @@
+// 98. Validate Binary Search Tree
+
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+    The left subtree of a node contains only nodes with keys less than the node's key.
+    The right subtree of a node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+
+Input:
+    2
+   / \
+  1   3
+Output: true
+
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+
+
+*/
+
+// 充分利用BST的性质. 左子树节点值小于根节点值, 右子树节点值大于跟节点
+// 因此可以判断左右子树节点的值的范围, 超过范围就不是合法的BST
+
+class Solution {
+public:
+  bool isValidBST(TreeNode *root) {
+    return isValidBST(root, nullptr, nullptr);
+  }
+private:
+  // root: 根节点, min: 指针, 指向左子节点的值, max: 指向右子节点的值
+  bool isValidBST(TreeNode *root, int *min, int *max) {
+    if (!root) return true;
+    if ((min && root->val <= *min) || (max && root->val >= *max))
+      return false;
+    return isValidBST(root->left, min, &root->val) && isValidBST(root->right, &root->val, max);
+  }
+};
