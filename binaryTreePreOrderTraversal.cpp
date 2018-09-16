7a626b43748b6aadbdf70b54a060decd2a3cd645
bigship <colinchen1984@gmail.com>
2018-09-16 11:29:58 +0800

    add problem 144. Binary Tree preorder traversal

---
+++ binaryTreePreOrderTraversal.cpp
@@ -0,0 +1,44 @@
+// 144. Binary Tree Preorder Traversal
+
+/*
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+// make use of stack. Because stack is LIFO, we should push right before push left
+
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> ans;
+    if (!root) return ans;
+    stack<TreeNode *> st;
+    st.push(root);
+
+    while (!st.empty()) {
+      TreeNode *node = st.top();
+      st.pop();
+      if (node) {
+        ans.push_back(node->val);
+        st.push(node->right);
+        st.push(node->left);
+      }
+    }
+
+    return ans;
+  }
+};
+
+
