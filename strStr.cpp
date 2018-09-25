aa782d166b010de848a90c215e4f35c7493fde3b
bigship <colinchen1984@gmail.com>
2018-09-25 21:31:53 +0800

    add problem 28. strStr

---
+++ strStr.cpp
@@ -0,0 +1,41 @@
+// 28. Implement strStr()
+
+/*
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+*/
+
+class Solution {
+public:
+  int strStr(string haystack, string needle) {
+    int x = needle.size();
+    if (x == 0) return 0;
+    for (int i = 0; i <= static_cast<int>(haystack.size())-x; i++) {
+      if (isSameString(haystack, needle, i)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+private:
+  bool isSameString(string& haystack, string& target, int start) {
+    return haystack.substr(start, target.size()) == target;
+  }
+};
