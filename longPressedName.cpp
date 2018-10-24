ff4c92454b31293b07cfeee6e54caf6ac52d5769
bigship <colinchen1984@gmail.com>
2018-10-24 15:37:39 +0800

    add problem 925. Long Pressed Name

---
+++ longPressedName.cpp
@@ -0,0 +1,63 @@
+// 925. Long Pressed Name
+
+/*
+Your friend is typing his name into a keyboard.  Sometimes, when typing a character c, the key might get long pressed, and the character 
+will be typed 1 or more times.
+
+You examine the typed characters of the keyboard.  Return True if it is possible that it was your friends name, with some characters 
+(possibly none) being long pressed.
+
+Example 1:
+
+Input: name = "alex", typed = "aaleex"
+Output: true
+Explanation: 'a' and 'e' in 'alex' were long pressed.
+
+Example 2:
+
+Input: name = "saeed", typed = "ssaaedd"
+Output: false
+Explanation: 'e' must have been pressed twice, but it wasn't in the typed output.
+
+Example 3:
+
+Input: name = "leelee", typed = "lleeelee"
+Output: true
+
+Example 4:
+
+Input: name = "laiden", typed = "laiden"
+Output: true
+Explanation: It's not necessary to long press any character.
+
+Note:
+
+    name.length <= 1000
+    typed.length <= 1000
+    The characters of name and typed are lowercase letters.
+
+*/
+
+class Solution {
+public:
+  bool isLongPressedName(string name, string typed) {
+    int i = 0, j = 0, count = 0;
+    while (i < name.size() || j < typed.size()) {
+      char ch = name[i];
+      count = 0;
+      while (ch == name[i] && i < name.size()) {
+        count++;
+        i++;
+      }
+      
+      while (ch == typed[j] && j < typed.size()) {
+        count--;
+        j++;
+      }
+      
+      if (count > 0) return false;
+    }
+    
+    return true;
+  }
+};
