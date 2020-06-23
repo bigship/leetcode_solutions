885e4827083f8347876fd9627fc2622b045101a0
bigship <colinchen1984@gmail.com>
2020-06-23 01:11:45 -0400

    add 63. UniquePathII

---
+++ 63_UniquePathII.cpp
@@ -0,0 +1,118 @@
+// 63. Unique Path II
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+*
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// Dynamic programming. Bottom Up
+int uniquePathWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int r = obstacleGrid.size();
+    if (r == 0) return 0;
+    int c = obstacleGrid[0].size();
+
+    // should use long, otherwise may overflow
+    vector<vector<long>> dp(r+1, vector<long>(c+1, 0));
+    dp[r-1][c] = 1;
+    for (int row = r-1; row >= 0; row--) {
+        for (int column = c-1; column >= 0; column--) {
+            dp[row][column] = (obstacleGrid[row][column] == 1) ? 0 : dp[row][column+1] + dp[row+1][column];
+        }
+    }
+    return dp[0][0];
+}
+
+// DFS + memorization
+int paths(int x, int y, vector<vector<int>>& f, const vector<vector<int>>& grid) {
+    // Out of bound, return 0
+    if (x <= 0 || y <= 0) return 0;
+
+    // Reaching the starting point
+    // There might be an obstacle here as well
+    if (x == 1 && y == 1) return 1 - grid[0][0];
+
+    // Already solved, return answer
+    if (f[y][x] != INT_MIN) return f[y][x];
+
+    // Obstacle on current block, no path
+    if (grid[y-1][x-1] == 1) {
+        f[y][x] = 0;
+    } else {
+        f[y][x] = paths(x-1, y, f, grid) + paths(x, y-1, f, grid);
+    }
+
+    return f[y][x];
+}
+
+int uniquePath(vector<vector<int>>& obstacleGrid) {
+    int n = obstacleGrid.size();
+    if (n == 0) return 0;
+    int m = obstacleGrid[0].size();
+
+    auto f = vector<vector<int>>(n+1, vector<int>(m+1, INT_MIN));
+    return paths(m, n, f, obstacleGrid);
+}
+
+
+int main() {
+    vector<vector<int>> grid = {
+        {0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,0,0,1,0,0},
+        {0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,0,0},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,1},
+        {0,0,0,1,0,0,0,0,0,0,0,1,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
+        {0,0,1,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,1,0,0},
+        {0,0,0,0,0,0,1,0,0,0,1,1,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,1,0,0,0,1},
+        {0,0,0,0,1,0,0,1,0,1,1,1,0,0,0,1,0,0,1,0,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0},
+        {1,0,1,0,1,1,0,1,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,1,0},
+        {0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0,0,1},
+        {0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,1,0,0,0},
+        {1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+        {0,1,0,0,1,0,0,0,0,0,1,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+        {0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0},
+        {0,0,0,1,0,1,0,0,1,0,0,0,0,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,1,1,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+        {1,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+        {0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0},
+        {0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,0,0,1,0,1,1,1,0,0,0,0,0,0,1,0,0,0,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0}
+    };
+
+    cout << "dp solution: " << uniquePathWithObstacles(grid) << endl;
+    cout << "dfs solution: " << uniquePath(grid) << endl;
+    return 0;
+}
+
