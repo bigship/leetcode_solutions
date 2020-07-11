56679c95a4502e9dd6507a2e8a480e24201dac52
bigship <colinchen1984@gmail.com>
2020-07-11 11:02:49 -0400

    add comments for find pivot index

--- findPivotIndex.cpp
+++ findPivotIndex.cpp
@@ -3,26 +3,26 @@
 /*
 Given an array of integers nums, write a method that returns the "pivot" index of this array.
 
-We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers 
+We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers
 to the right of the index.
 
 If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
 
 Example 1:
 
-Input: 
+Input:
 nums = [1, 7, 3, 6, 5, 6]
 Output: 3
-Explanation: 
+Explanation:
 The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
 Also, 3 is the first index where this occurs.
 
 Example 2:
 
-Input: 
+Input:
 nums = [1, 2, 3]
 Output: -1
-Explanation: 
+Explanation:
 There is no index that satisfies the conditions in the problem statement.
 
 Note:
@@ -32,6 +32,9 @@ Each element nums[i] will be an integer in the range [-1000, 1000].
 
 class Solution {
 public:
+  // totalsum = left + right + nums[i]
+  // since left == right, we have totalsum - nums[i] == 2 * left
+  // We could do it in linear time
   int pivotIndex(vector<int>& nums) {
     int sum = 0, leftsum = 0;
     sum = std::accumulate(nums.begin(), nums.end(), 0);
