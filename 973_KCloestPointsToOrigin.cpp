6e40f61e22a086e0d0d02cca433e3604dbe31e96
bigship <colinchen1984@gmail.com>
2020-07-17 13:29:14 -0400

    add 973 K cloest points to origin

---
+++ 973_KCloestPointsToOrigin.cpp
@@ -0,0 +1,51 @@
+// 973. K closest Points to Origin
+/*
+ * We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+
+
+
+Note:
+
+    1 <= K <= points.length <= 10000
+    -10000 < points[i][0] < 10000
+    -10000 < points[i][1] < 10000
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        auto cmp = [&](vector<int>&left, vector<int>& right) {
+            return static_cast<int>(pow(left[0], 2) + pow(left[1], 2)) <
+                static_cast<int>(pow(right[0], 2) + pow(right[1], 2));
+        }
+        std::sort(points.begin(), points.end(), cmp);
+        vector<vector<int>> ans;
+        for (int i = 0; i < K; i++) {
+            ans.push_back(points[i]);
+        }
+        return ans;
+    }
+};
