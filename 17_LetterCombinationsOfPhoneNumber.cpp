a749cbe89547c497528691b0263f3847532841c0
bigship <colinchen1984@gmail.com>
2020-07-30 23:31:35 -0400

    add 17 Letter Combinations of phone number

---
+++ 17_LetterCombinationsOfPhoneNumber.cpp
@@ -0,0 +1,44 @@
+// 17. Letter Combinations of a Phone Number
+/*
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations
+ * that the number could represent.
+ *
+   A mapping of digit to letters (just like on the telephone buttons) is given below.
+   Note that 1 does not map to any letters.
+
+Example:
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+*/
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if (digits.size() != 0)
+            backtrack(ans, "", digits, 0);
+        return ans;
+    }
+
+private:
+    unordered_map<char, string> m = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
+        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+    };
+
+    void backtrack(vector<string>& ans, string tmp, string digits, int start) {
+        if (digits.size() == tmp.size()) {
+            ans.push_back(tmp);
+            return ;
+        }
+        string tp = m[digits[start]];
+        for (int i = 0; i < tp.size(); ++i) {
+            backtrack(ans, tmp+tp[i], digits, start+1);
+        }
+    }
+};
+
+
