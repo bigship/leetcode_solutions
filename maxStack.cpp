e2304a9472951b25581c04c112695abd8be6469c
bigship <colinchen1984@gmail.com>
2020-10-19 23:51:13 -0400

    add doublely linked list + map solution

--- maxStack.cpp
+++ maxStack.cpp
@@ -6,13 +6,13 @@ Design a max stack that supports push, pop, top, peekMax and popMax.
     pop() -- Remove the element on top of the stack and return it.
     top() -- Get the element on the top.
     peekMax() -- Retrieve the maximum element in the stack.
-    popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, 
+    popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements,
     only remove the top-most one.
 
 Example 1:
 
 MaxStack stack = new MaxStack();
-stack.push(5); 
+stack.push(5);
 stack.push(1);
 stack.push(5);
 stack.top(); -> 5
@@ -29,6 +29,7 @@ Note:
     The last four operations won't be called when stack is empty.
 */
 
+// peekMax - O(n), popMax - O(n)
 class MaxStack {
 public:
   MaxStack() {
@@ -66,4 +67,46 @@ public:
   }
 private:
   deque<int> st;
+};
+
+
+// Doublely linked list + map
+class MaxStack {
+public:
+  MaxStack() {
+
+  }
+
+  void push(int x) {
+    sk.push_back(x);
+    m[x].push_back(--sk.end());
+  }
+
+  int pop() {
+    int v = sk.back();
+    m[v].pop_back();
+    if (m[v].empty())
+      m.erase(v);
+    return v;
+  }
+
+  int top() {
+    return sk.back();
+  }
+
+  int peekMax() {
+    return m.rbegin()->first;
+  }
+
+  int popMax() {
+    int v = peekMax();
+    auto it = m[v].back();
+    sk.erase(it);
+    m[v].pop_back();
+    if (m[v].empty())
+      m.erase(v);
+  }
+private:
+  std::list<int> sk;
+  std::map<int, std::vector<std::list<int>::iterator>> m;
 };
