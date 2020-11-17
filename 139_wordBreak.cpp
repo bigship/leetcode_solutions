b8f6214fc5bea146b84f977dbf57ac5bdaf5bf9c
bigship <colinchen1984@gmail.com>
2020-11-17 11:28:53 -0500

    add recursion + memorization solution

--- 139_wordBreak.cpp
+++ 139_wordBreak.cpp
@@ -37,55 +37,56 @@ using namespace std;
 
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        bool ans = false;
-        helper(s, s.size(), wordDict, ans);
-        return ans;
-    }
-private:
-    // DFS TLE !!
-    void helper(string str, size_t len, vector<string>& wordDict, bool& ans) {
-        if (len == 0) {
-            ans = true;
-            return ;
-        } else {
-            for (auto& w : wordDict) {
-                if (str.find(w) == 0)
-                    helper(str.substr(w.size()), str.size() - w.size(), wordDict, ans);
-            }
-        }
-    }
+  // Dynamic Programming
+  bool wordBreakDP(string s, vector<string>& wordDict) {
+    unordered_set<string> dict(wordDict.cbegin(), wordDict.cend());
+    int n = s.length();
+    s = " " + s;  // s[1..n]
+    vector<int> f(n+1, 0);
+    f[0] = 1;
 
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (f[j] == 1) {
+          const string news = s.substr(j+1, i-j);  // s[j+1..i]
+          if (dict.count(news)) {
+            f[i] = 1;
+            break;
+          }
+        }
+      }
+    }
+    return f[n];
+  }
+};
+
+class Solution {
 public:
-    // Dynamic Programming
-    bool wordBreakDP(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.cbegin(), wordDict.cend());
-        int n = s.length();
-        s = " " + s;  // s[1..n]
-        vector<int> f(n+1, 0);
-        f[0] = 1;
+  bool wordBreak(string s, vector<string>& wordDict) {
+    // vector转hashset, O(1)时间判断是否存在dict中
+    unordered_set<string> dict(wordDict.begin(), wordDict.end());
+    return wordBreak(s, dict);
+  }
+private:
+  unordered_map<string, bool> mem_;  // 记忆化, 保存求解过的解
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (f[j] == 1) {
-                    const string news = s.substr(j+1, i-j);  // s[j+1..i]
-                    if (dict.count(news)) {
-                        f[i] = 1;
-                        break;
-                    }
-                }
-            }
-        }
-        return f[n];
+  bool wordBreak(const string& s, const unordered_set<string>& dict) {
+    if (mem_.count(s)) return mem_[s];   // 已经求解过了直接返回
+    if (dict.count(s)) return mem_[s] = true;  // 特例, 整个单词刚好就在字典里, 返回true
+
+    // 遍历每个分隔点
+    for (int j = 1; j < s.length(); j++) {
+      const string left = s.substr(0, j);
+      const string right = s.substr(j);
+
+      // 如果右边存在于字典, 且左边也可以分隔则表示找到了解
+      if (dict.count(right) && wordBreak(left, dict))
+        return mem_[s] = true;
     }
+
+    // 如果每个分隔点都尝试了, 但找不到解, 则表示无解
+    return mem_[s] = false;
+  }
 };
 
 
-int main() {
-    string s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
-    //string s = "aaaaaab";
-    vector<string> wordDict{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
-    //cout << "ans: " << Solution().wordBreak(s, wordDict) << endl;
-    cout << "ans: " << std::boolalpha << Solution().wordBreakDP(s, wordDict) << endl;
-    return 0;
-}
