672e1ce59ee29452803bf3dfca0ae28354922e71
bigship <colinchen1984@gmail.com>
2020-12-25 00:14:44 -0500

    add recursive solution

--- 78_Subsets.cpp
+++ 78_Subsets.cpp
@@ -50,3 +50,25 @@ private:
   }
 };
 
+class Solution {
+public:
+  // 递归做法
+  // subset([1,2,3]) = subset([1, 2]) + [A[i].add(3) for i = 1 .. len(A)]
+  vector<vector<int>> subsets(vector<int>& nums) {
+    if (nums.empty()) return {{}};
+
+    // 取出最后一个元素
+    int n = nums.back();
+    nums.pop_back();
+
+    // 先递归算出前面元素的所有子集
+    vector<vector<int>> res = subsets(nums);
+
+    int size = res.size();
+    for (int i = 0; i < size; i++) {
+      res.push_back(res[i]);
+      res.back().push_back(n);
+    }
+    return res;
+  }
+};
