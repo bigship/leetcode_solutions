f45c03831ddf76766d83865af6946f1548c8d39f
bigship <colinchen1984@gmail.com>
2021-01-20 16:11:41 -0500

    add iterative solution for Integer To Roman

--- 12_IntegerToRoman.cpp
+++ 12_IntegerToRoman.cpp
@@ -54,109 +54,45 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 class Solution {
 public:
-    // Too complex, error pron, bad implementation
-    string intToRoman(int num) {
-        string ans = "";
-        deque<int> q;
-        while (num) {
-            q.push_front(num % 10);
-            num /= 10;
-        }
-        int sz = q.size();
-        while (sz < 4) {
-            q.push_front(0);
-            sz++;
-        }
+  string intToRoman(int num) {
+    if (num >= 1000) return ("M" + intToRoman(num - 1000));
+    if (num >= 900) return ("CM" + intToRoman(num - 900));
+    if (num >= 500) return ("D" + intToRoman(num - 500));
+    if (num >= 400) return ("CD" + intToRoman(num - 400));
+    if (num >= 100) return ("C" + intToRoman(num - 100));
+    if (num >= 90) return ("XC" + intToRoman(num - 90));
+    if (num >= 50) return ("L" + intToRoman(num - 50));
+    if (num >= 40) return ("XL" + intToRoman(num - 40));
+    if (num >= 10) return ("X" + intToRoman(num - 10));
+    if (num == 9) return "IX";
+    if (num == 8) return "VIII";
+    if (num == 7) return "VII";
+    if (num == 6) return "VI";
+    if (num == 5) return "V";
+    if (num == 4) return "IV";
+    if (num == 3) return "III";
+    if (num == 2) return "II";
+    if (num == 1) return "I";
+    if (num == 0) return "";
+    return "";
+  }
+};
 
-        for (int i = 0; i < q.size(); i++) {
-            if (i == 0 && q[i] != 0) {   // thousand
-                while (q[i] > 0) {
-                    ans += "M";
-                    q[i]--;
-                }
-            }
+class Solution2 {
+public:
+  string intToRoman(int num) {
+    vector<int> values{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> symbols{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-            if (i == 1 && q[i] != 0) {   // hundred
-                if (q[i] == 5) ans = ans + "D";
-                if (q[i] == 4) ans = ans + "CD";
-                if (q[i] == 9) ans = ans + "CM";
-                if (q[i] > 5 && q[i] < 9) {
-                    ans = ans + "D";
-                    while (q[i] - 5 > 0) {
-                        ans = ans + "C";
-                        q[i]--;
-                    }
-                } else if (q[i] > 0 && q[i] < 4) {
-                    while (q[i] > 0) {
-                        ans = ans + "C";
-                        q[i]--;
-                    }
-                }
-            }
-
-            if (i == 2 && q[i] != 0) {  // ten
-                if (q[i] == 5) ans = ans + "L";
-                if (q[i] == 4) ans = ans + "XL";
-                if (q[i] == 9) ans = ans + "XC";
-                if (q[i] > 5 && q[i] < 9) {
-                    ans = ans + "L";
-                    while (q[i] - 5 > 0) {
-                        ans = ans + "X";
-                        q[i]--;
-                    }
-                } else if (q[i] > 0 && q[i] < 4) {
-                    ans = ans + "X";
-                    while (q[i] > 0) {
-                        ans = ans + "X";
-                        q[i]--;
-                    }
-                }
-            }
-
-            if (i == 3 && q[i] != 0) {
-                if (q[i] == 5) ans = ans + "V";
-                if (q[i] == 9) ans = ans + "IX";
-                if (q[i] == 4) ans = ans + "IV";
-                if (q[i] > 5 && q[i] < 9) {
-                    ans = ans + "V";
-                    while (q[i] - 5 > 0) {
-                        ans = ans + "I";
-                        q[i]--;
-                    }
-                } else if (q[i] > 0 && q[i] < 4) {
-                    while (q[i] > 0) {
-                        ans = ans + "I";
-                        q[i]--;
-
-                    }
-                }
-            }
-        }
-        return ans;
+    string res;
+    for (int i = 0; i < values.size() && num >= 0; i++) {
+      while (values[i] <= num) {
+        num -= values[i];
+        res += symbols[i];
+      }
     }
-
-    // Recursion, much more elegant
-    string intToRoman(int num) {
-        if (num >= 1000) return ("M" + intToRoman(num - 1000));
-        if (num >= 900) return ("CM" + intToRoman(num - 900));
-        if (num >= 500) return ("D" + intToRoman(num - 500));
-        if (num >= 400) return ("CD" + intToRoman(num - 400));
-        if (num >= 100) return ("C" + intToRoman(num - 100));
-        if (num >= 90) return ("XC" + intToRoman(num - 90));
-        if (num >= 50) return ("L" + intToRoman(num - 50));
-        if (num >= 40) return ("XL" + intToRoman(num - 40));
-        if (num >= 10) return ("X" + intToRoman(num - 10));
-        if (num == 9) return "IX";
-        if (num == 8) return "VIII";
-        if (num == 7) return "VII";
-        if (num == 6) return "VI";
-        if (num == 5) return "V";
-        if (num == 4) return "IV";
-        if (num == 3) return "III";
-        if (num == 2) return "II";
-        if (num == 1) return "I";
-        if (num == 0) return "";
-        return "";
+    return res;
+  }
 };
 
 
