3da733ce5125bada65d953ad61a6e050e5998672
bigship <colinchen1984@gmail.com>
2021-03-25 19:52:48 -0400

    add 127 bi-directional BFS solution

--- 127_wordLadder.cpp
+++ 127_wordLadder.cpp
@@ -78,3 +78,47 @@ int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
     return 0;
 }
 
+// 双向BFS
+class Solution {
+public:
+  int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    if (!dict.count(endWord)) return 0;
+    unordered_set<string> dict(wordList.begin(), wordList.end());
+
+    int l = beginWord.length();
+
+    unordered_set<string> q1{beginWord};
+    unordered_set<string> q2{endWord};
+
+    int step = 0;
+
+    while (!q1.empty() && !q2.empty()) {
+      ++step;
+
+      // Always expend the smaller queue first
+      if (q1.size() > q2.size())
+        std::swap(q1, q2);
+
+      unordered_set<string> q;
+
+      for (string w : q1) {
+        for (int i = 0; i < l; i++) {
+          char ch = w[i];
+          for (int j = 'a'; j <= 'z'; j++) {
+            w[i] = j;
+            if (q2.count(w)) return step + 1;
+            if (!dict.count(w)) continue;
+            dict.erase(w);
+            q.insert(w);
+          }
+          w[i] = ch;
+        }
+      }
+
+      std::swap(q, q1);
+    }
+
+    return 0;
+  }
+};
+
